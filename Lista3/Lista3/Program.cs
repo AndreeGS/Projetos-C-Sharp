bde4b49b00536bc50b572c2276aa3f07c608d842
AndreeGS <andre.guilherme2009@gmail.com>
2023-12-21 20:19:06 -0300

    Novo Projeto

---
+++ Lista3/Lista3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Sistema;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+       Menu menu = new Menu();
+       menu.exeMenu();
+    }
+
+}
+
+
+

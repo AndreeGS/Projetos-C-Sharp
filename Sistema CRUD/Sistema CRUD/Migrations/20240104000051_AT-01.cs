e9e494d45aa2a7435b701d5b96afbbbb989d1ffa
AndreeGS <andre.guilherme2009@gmail.com>
2024-01-03 21:05:29 -0300

    Atualização

---
+++ Sistema CRUD/Sistema CRUD/Migrations/20240104000051_AT-01.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_CRUD.Migrations
+{
+    public partial class AT01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
--- Sistema CRUD/Sistema CRUD/Models/AppDbContextcs.cs
+++ Sistema CRUD/Sistema CRUD/Models/AppDbContextcs.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Sistema_CRUD.Models
 {
-    public class AppDbContextcs : DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        {
-        }
-    }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    
+        public DbSet<Client> Clientes { get; set; }
+    }   
 }

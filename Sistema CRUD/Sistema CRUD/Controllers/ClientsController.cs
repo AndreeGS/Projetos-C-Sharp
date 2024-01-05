bbff224403333f8e88ca978d7d2672c20bd68aa6
AndreeGS <andre.guilherme2009@gmail.com>
2024-01-04 21:49:40 -0300

    Atualização View

---
+++ Sistema CRUD/Sistema CRUD/Controllers/ClientsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_CRUD.Models;
+
+namespace Sistema_CRUD.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientsController(AppDbContext context)
+        { 
+            _context = context;
+        } 
+        
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Clientes.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
